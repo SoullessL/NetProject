07705e8452b231a9ae40c45390d292a836d936c9
SoullessL home <soullessl@qq.com>
2016-11-19 18:09:07 +0800

    Lazy demo

---
+++ NetTrain/LazyDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<PiKaQiu> pi = new Lazy<PiKaQiu>();
+            Console.WriteLine(pi);
+            pi.Value.Name = "Jay";
+            pi.Value.Show();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class PiKaQiu
+    {
+        public string Name { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine("This is PiKaQiu:" + this.Name);
+        }
+    }
+}
